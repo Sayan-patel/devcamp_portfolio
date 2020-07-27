4a926aed98e09a7814751590676c2ee8007076cb
Sayan Patel <sayanpatel@Sayan-Patels-2020-MacBook-Pro.local>
2020-07-27 22:51:56 +0530

    Implemented default for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'My great service')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/300"
+		self.thumb_image ||= "https://via.placeholder.com/150"
+	end
 end
