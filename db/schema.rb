8c8c4ef56191ce57bd6f397875245cc54338e05e
Sayan Patel <sayanpatel@Sayan-Patels-2020-MacBook-Pro.local>
2020-07-28 09:42:50 +0530

    Implemented concerns for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_140903) do
+ActiveRecord::Schema.define(version: 2020_07_28_035026) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_140903) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
