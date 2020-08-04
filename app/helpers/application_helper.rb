dd54ae457faf69808f0e23910a641d4a3c6fc527
Sayan Patel <sayanpatel@Sayan-Patels-2020-MacBook-Pro.local>
2020-08-04 18:58:11 +0530

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
 	def login_helper
-		if current_user.is_a?(User) 
-    	  link_to "Logout", destroy_user_session_path, method: :delete
-    	else 
-    	  (link_to "Register", new_user_registration_path) + 
-    	  "<br>".html_safe + 
-    	  (link_to "Login", new_user_session_path) 
+		if current_user.is_a?(GuestUser)
+            (link_to "Register", new_user_registration_path) + 
+            "<br>".html_safe +
+            (link_to "Login", new_user_session_path)
+    	else
+            link_to "Logout", destroy_user_session_path, method: :delete   
     	end
     end
 
@@ -22,6 +22,6 @@ module ApplicationHelper
   	end
 
     def copyright_generator
-        SayanViewTool::Renderer.copyright 'Sayan patel', 'All rights reserved'
+        SayanViewTool::Renderer.copyright 'Sayan patel', 'All rights reserved mast'
     end
 end
