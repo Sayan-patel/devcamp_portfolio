443cb6495c873eb0696078719e842faa49cc358f
Sayan Patel <sayanpatel@Sayan-Patels-2020-MacBook-Pro.local>
2020-08-10 23:39:10 +0530

    Implemented gritter for notifications

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -77,7 +77,7 @@ class PortfoliosController < ApplicationController
   private 
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body,:thumb_image, :main_image, technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title, :subtitle, :body,:thumb_image, :main_image, technologies_attributes: [:id,:_destroy,:name])
   end
 
   def set_portfolio_item
